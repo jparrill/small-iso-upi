5bbfa18669a85ae516f4177f04a6e24e59bbb8fb
Mario Vazquez <mavazque@redhat.com>
2020-11-24 17:49:22 +0100

    Define installation disk on the csv

--- iso-utils/iso.sh
+++ iso-utils/iso.sh
@@ -2,18 +2,11 @@
 firstboot_args='console=tty0 rd.neednet=1'
 for vg in $(vgs -o name --noheadings) ; do vgremove -y $vg ; done
 for pv in $(pvs -o name --noheadings) ; do pvremove -y $pv ; done
-if [ -b /dev/vda ]; then
-  install_device='/dev/vda'
-elif [ -b /dev/sda ] && [ "$(lsblk /dev/sda)" != "" ] ; then
-  install_device='/dev/sda'
-elif [ -b /dev/sdb ] && [ "$(lsblk /dev/sdb)" != "" ] ; then
-  install_device='/dev/sdb'
-elif [ -b /dev/sdc ] && [ "$(lsblk /dev/sdc)" != "" ] ; then
-  install_device='/dev/sdc'
-elif [ -b /dev/sdd ] && [ "$(lsblk /dev/sdd)" != "" ] ; then
-  install_device='/dev/sdd'
-elif [ -b /dev/nvme0 ]; then
-  install_device='/dev/nvme0'
+
+install_device=$(cat /tmp/installation_disk)
+
+if [ -b /dev/${install_device} ]; then
+  install_device="/dev/${install_device}"
 else
   echo "Can't find appropriate device to install to"
   exit 1
