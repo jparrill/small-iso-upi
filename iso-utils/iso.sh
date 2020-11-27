3a63b8148df9bd577155f2cea8681e8a76d3c5cb
Mario Vazquez <mavazque@redhat.com>
2020-11-27 15:31:02 +0100

    updated iso.sh script

--- iso-utils/iso.sh
+++ iso-utils/iso.sh
@@ -3,10 +3,25 @@ firstboot_args='console=tty0 rd.neednet=1'
 for vg in $(vgs -o name --noheadings) ; do vgremove -y $vg ; done
 for pv in $(pvs -o name --noheadings) ; do pvremove -y $pv ; done
 
-install_device=$(cat /tmp/installation_disk)
-
-if [ -b /dev/${install_device} ]; then
-  install_device="/dev/${install_device}"
+if [ -f /tmp/installation_disk ]; then
+  install_device_file=$(cat /tmp/installation_disk)
+  install_device="/dev/${install_device_file}"
+  if [ ! -b ${install_device} ]; then
+    echo "Can't find device ${install_device}, trying to guess install device now"
+    exit 1
+  fi
+elif [ -b /dev/vda ]; then
+  install_device='/dev/vda'
+elif [ -b /dev/sda ] && [ "$(lsblk /dev/sda)" != "" ] ; then
+  install_device='/dev/sda'
+elif [ -b /dev/sdb ] && [ "$(lsblk /dev/sdb)" != "" ] ; then
+  install_device='/dev/sdb'
+elif [ -b /dev/sdc ] && [ "$(lsblk /dev/sdc)" != "" ] ; then
+  install_device='/dev/sdc'
+elif [ -b /dev/sdd ] && [ "$(lsblk /dev/sdd)" != "" ] ; then
+  install_device='/dev/sdd'
+elif [ -b /dev/nvme0 ]; then
+  install_device='/dev/nvme0'
 else
   echo "Can't find appropriate device to install to"
   exit 1
